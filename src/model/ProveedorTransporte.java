c946c934eda98a96d9cdbd9e1820048cd5b0d65c
fiscfelipe <fisc.felipe@gmail.com>
2026-06-08 20:36:57 -0400

    Update ProveedorTransporte.java

--- src/model/ProveedorTransporte.java
+++ src/model/ProveedorTransporte.java
@@ -1,75 +1,45 @@
 package model;
+
 /**
  * Clase que representa una Persona que es Proveedor de Transporte para LlanquihueTour
- * 
- * Esta clase hereda de la clase Persona, ya que todos los Proveedores de Transporte son Personas
- * * Esta clase utiliza composición, ya que todo Proveedor de transporte, registra una patente
+ *
+ * Esta clase hereda de la clase Persona.
+ * Esta clase utiliza composición, ya que un proveedor registra un vehículo.
  */
 public class ProveedorTransporte extends Persona {
-
+    
     //Atributos propios
-    private String tipoVehiculo;
-    private Patente patenteVehiculo;
-    private int asientosDisponibles;
+    private Vehiculo vehiculo;
 
     /**
-     * Constructor de la clase ProveedorTransporte 
      * 
-     * @param nombre                        Nombre del Proveedor de Transporte
-     * @param rut                           Rut del Proveedor de Transporte
-     * @param correo                        Correo electrónico del Proveedor de Transporte
-     * @param direccion                     Dirección del domicilio del Proveedor de Transporte
-     * @param tipoVehiculo                  Tipo de vehículo que maneja el Proveedor de Transporte
-     * @param patenteVehiculo               Patente del vehículo que maneja el Proveedor de Transporte
-     * @param asientosDisponibles           Asientos disponibles que tiene en su vehiculo el Proveedor de Transporte
+     * @param nombre        Nombre del proveedor de transporte
+     * @param rut           RUT del proveedor de transporte
+     * @param correo        Correo del proveedor de transporte
+     * @param direccion     Direccion del proveedor de transporte
+     * @param vehiculo      Vehiculo registrado a nombre del proveedor de transporte
      */
-    public ProveedorTransporte( String nombre, Rut rut, Correo correo, Direccion direccion, String tipoVehiculo, Patente patenteVehiculo, int asientosDisponibles) {
+    public ProveedorTransporte(String nombre, Rut rut, Correo correo, Direccion direccion, Vehiculo vehiculo) {
 
         super(nombre, rut, correo, direccion);
-
-        this.tipoVehiculo = tipoVehiculo;
-        this.patenteVehiculo = patenteVehiculo;
-        this.asientosDisponibles = asientosDisponibles;
+        this.vehiculo = vehiculo;
     }
 
     //Setters
-    public void setTipoVehiculo(String tipoVehiculo) {
-        this.tipoVehiculo = tipoVehiculo;
-    }
-
-    public void setPatenteVehiculo(Patente patenteVehiculo) {
-        this.patenteVehiculo = patenteVehiculo;
-    }
-
-    public void setAsientosDisponibles(int asientosDisponibles) {
-        this.asientosDisponibles = asientosDisponibles;
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
     }
 
     //Getters
-    public String getTipoVehiculo() {
-        return tipoVehiculo;
-    }
-
-    public Patente getPatenteVehiculo() {
-        return patenteVehiculo;
-    }
-
-    public int getAsientosDisponibles() {
-        return asientosDisponibles;
+    public Vehiculo getVehiculo() {
+        return vehiculo;
     }
 
     //Métodos
-    
-    /**
-     * Sobreescribe el método toString()
-     * @return lo mismo que el toString() de persona más sus atributos propios anexados
-     */
     @Override
     public String toString() {
 
         return super.toString() +
-                "\nTipo vehiculo: " + tipoVehiculo +
-                "\nPatente vehiculo: " + patenteVehiculo +
-                "\nAsientos disponibles: " + asientosDisponibles;
+                "\n" + vehiculo;
     }
 }
